4b00e869a5dbc192849667137c48dee540244649
nanato12 <admin@nanato12.info>
2021-06-16 13:11:20 +0900

    Update color

--- term_printer/consts/color.py
+++ term_printer/consts/color.py
@@ -27,6 +27,14 @@ class Color(Enum):
     CYAN = 36
     WHITE = 37
     DEFAULT = 39
+    LIGHT_GRAY = 90
+    LIGHT_RED = 91
+    LIGHT_GREEN = 92
+    LIGHT_YELLOW = 93
+    LIGHT_BLUE = 94
+    LIGHT_MAGENTA = 95
+    LIGHT_CYAN = 96
+    LIGHT_WHITE = 97
     BG_BLACK = 40
     BG_RED = 41
     BG_GREEN = 42
@@ -36,3 +44,38 @@ class Color(Enum):
     BG_CYAN = 46
     BG_WHITE = 47
     BG_DEFAULT = 49
+
+
+class Color256:
+    __n: int
+    is_bg: bool
+
+    def __init__(self, n: int, is_bg: bool = False) -> None:
+        if not (0 <= n <= 255):
+            raise Exception(f"Invalid 256bit color number: {n}")
+        self.__n = n
+        self.is_bg = is_bg
+
+    def __str__(self) -> str:
+        tag: str = "48" if self.is_bg else "38"
+        return f"{tag};5;{self.__n}"
+
+
+class ColorRGB:
+    __R: int
+    __G: int
+    __B: int
+    is_bg: bool
+
+    def __init__(self, R: int, G: int, B: int, is_bg: bool = False) -> None:
+        for k, n in {"R": R, "G": G, "B": B}.items():
+            if not (0 <= n <= 255):
+                raise Exception(f"Invalid 256bit color number: {k}:{n}")
+        self.__R = R
+        self.__G = G
+        self.__B = B
+        self.is_bg = is_bg
+
+    def __str__(self) -> str:
+        tag: str = "48" if self.is_bg else "38"
+        return f"{tag};2;{self.__R};{self.__G};{self.__B}"
